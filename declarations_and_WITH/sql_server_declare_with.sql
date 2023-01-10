664952b624b49d0e4abe70331a2f6970c02a70bc
Jordan Frey <36864344+FreyGeospatial@users.noreply.github.com>
2023-01-10 10:59:06 -0500

    Create sql_server_declare_with.sql

---
+++ declarations_and_WITH/sql_server_declare_with.sql
@@ -0,0 +1,9 @@
+DECLARE @my_int AS INT;
+-- USE SET OR SELECT
+SET @my_int = 3;
+SET @my_int = @my_int + 1
+select @my_int;	
+select @my_int + 1
+select @my_int
+select @my_int=@my_int+1
+select @my_int
