d8e50081501318abd2316bd9b910d91df744fa99
Jordan Frey <36864344+FreyGeospatial@users.noreply.github.com>
2023-03-14 12:34:20 -0400

    Create updating_json.sql

---
+++ json/updating_json.sql
@@ -0,0 +1,15 @@
+-- for postgres / redshift
+-----------------------------------------
+create table mytable(
+  id int primary key,
+  test_json super
+);
+
+insert into mytable (id, test_json)
+values (1, '{"house": {"bedroom": ["bed", "nightstand"]}}'::super);
+
+insert into mytable (id)
+values (2);
+
+update mytable
+set test_json = json_parse('{"house": {"living_room": ["couch", "television"]:}}');
